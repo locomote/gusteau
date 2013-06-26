ebc0033358775ee0074928b886ec9d501faff787
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-06-27 06:16:06 +1000

    Fix ssh_config to only include server nodes

--- lib/gusteau/ssh_config.rb
+++ lib/gusteau/ssh_config.rb
@@ -4,12 +4,16 @@ require 'gusteau/server'
 module Gusteau
   class SSHConfig
     def initialize(root_dir = ".")
-      @config = Dir.glob("#{root_dir}/nodes/**/*.yml").sort.map do |n|
-        name   = File.basename(n, '.*')
-        config = YAML::load_file(n)['server']
+      @config = []
 
-        section name, Gusteau::Server.new(config)
-      end.join("\n")
+      Dir.glob("#{root_dir}/nodes/**/*.yml").sort.each do |n|
+        name   = File.basename(n, '.*')
+        config = YAML::load_file(n)
+
+        if server = config['server']
+          @config << section(name, Gusteau::Server.new(server))
+        end
+      end
     end
 
     def section(name, server)
@@ -25,7 +29,7 @@ Host #{name}
       <<-eos
 # BEGIN GUSTEAU NODES
 
-#{@config}
+#{@config.join("\n")}
 # END GUSTEAU NODES
       eos
     end
