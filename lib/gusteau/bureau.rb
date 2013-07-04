6641e4f0314940c5317104021eadf3c2b370c724
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-07-04 14:14:34 +1000

    Implement Bureau generation tests

--- lib/gusteau/bureau.rb
+++ lib/gusteau/bureau.rb
@@ -8,37 +8,53 @@ module Gusteau
     include Gusteau::ERB
 
     def initialize(name)
-      template_path = File.expand_path('../../../template', __FILE__)
+      @name = name
+      @template_path = File.expand_path('../../../template', __FILE__)
 
       @login   = Etc.getlogin
       @ssh_key = File.read(File.expand_path '~/.ssh/id_rsa.pub').chomp rescue 'Your SSH key'
 
       abort "Directory #{name} already exists" if Dir.exists?(name)
+    end
 
-      FileUtils.cp_r(template_path, name)
+    def generate!(init = true)
+      FileUtils.cp_r(@template_path, @name)
+      yaml_template '.gusteau.yml'
+      text_template 'README.md'
+      json_template "data_bags/users/#{@login}.json", "data_bags/users/user.json.erb"
+      Dir.chdir(name) { system "bash ./init.sh ; rm ./init.sh" } if(init)
+    end
 
-      File.open(File.join(name, '.gusteau.yml'), 'w+') do |f|
-        read_erb_yaml(File.join(template_path, '.gusteau.yml.erb')).tap do |node|
-          f.write node.to_yaml
-          f.close
-        end
+    private
 
-        FileUtils.rm(File.join(name, '.gusteau.yml.erb'))
-      end
-
-      File.open(File.join(name, 'data_bags', 'users', "#{@login}.json"), 'w+') do |f|
-        read_erb_json(File.join(template_path, 'data_bags', 'users', 'user.json.erb')).tap do |user|
-          f.write JSON::pretty_generate user
-          f.close
-        end
-
-        FileUtils.rm(File.join(name, 'data_bags', 'users', 'user.json.erb'))
-      end
-
-      puts "Created bureau '#{name}'"
-      Dir.chdir(name) do
-        system 'bash ./chop.sh ; rm ./chop.sh'
+    def yaml_template(file)
+      replace_template file do |f|
+        read_erb_yaml("#{@template_path}/#{file}.erb").tap { |c| f.write(c.to_yaml) }
       end
     end
+
+    def json_template(file, src)
+      replace_template file, src do |f|
+        read_erb_json("#{@template_path}/#{src}").tap { |c| f.write JSON::pretty_generate(c) }
+      end
+    end
+
+    def text_template(file)
+      replace_template file do |f|
+        read_erb("#{@template_path}/#{file}.erb").tap { |t| f.write t }
+      end
+    end
+
+    def replace_template(file, src = nil)
+      dest = "#{@name}/#{file}"
+      src  = "#{@name}/#{src}" if(src)
+
+      File.open(dest, 'w+') do |f|
+        yield f
+        f.close
+        FileUtils.rm(src || "#{dest}.erb")
+      end
+    end
+
   end
 end
--- spec/lib/gusteau/bureau_spec.rb
+++ spec/lib/gusteau/bureau_spec.rb
@@ -1,5 +1,52 @@
 require './spec/spec_helper'
+require 'fileutils'
+require 'etc'
 
 describe Gusteau::Bureau do
+
+  let(:login) { Etc.getlogin }
+  let(:bureau_path) { "/tmp/gusteau-test/bureau" }
+
+  before { FileUtils.mkdir_p("/tmp/gusteau-test") }
+  after  { FileUtils.rm_rf(bureau_path) }
+
+  subject { Gusteau::Bureau.new(bureau_path) }
+
+  it "should obtain the correct user login" do
+    subject.instance_variable_get("@login").must_equal login
+  end
+
+  describe "#generate!" do
+    before { subject.generate!(false) }
+    it "should create a basic structure" do
+      assert Dir.exists?(bureau_path)
+
+      %w{ Berksfile Vagrantfile .kitchen.yml }.each do |f|
+        assert File.exists?("#{bureau_path}/#{f}")
+      end
+
+      %w{ data_bags site-cookbooks test }.each do |d|
+        assert Dir.exists?("#{bureau_path}/#{d}")
+      end
+    end
+
+    it "should process the README template" do
+      readme_path = "#{bureau_path}/README.md"
+      assert File.exists?(readme_path)
+      assert File.read(readme_path).include?("Welcome to your example Chef-Repo, #{login}")
+    end
+
+    it "should process the .gusteau.yml template" do
+      config_path = "#{bureau_path}/.gusteau.yml"
+      assert File.exists?(config_path)
+      assert File.read(config_path).include?("Good job, #{login}")
+    end
+
+    it "should process the user data_bag template" do
+      config_path = "#{bureau_path}/data_bags/users/#{login}.json"
+      assert File.exists?(config_path)
+      assert File.read(config_path).include?(login)
+    end
+  end
 
 end
